29ff2a1577f1983b0d35cff885fec771b1850585
Czarek6688 <czarekrosloniec2006@gmail.com>
2026-07-27 23:28:41 +0200

    local storage

--- js/main.js
+++ js/main.js
@@ -7,6 +7,37 @@ let price = document.getElementById("price");
 let carList = document.getElementById("car-list");
 let searchInput = document.getElementById("search-car");
 
+const displayData = function () {
+  const cars = JSON.parse(localStorage.getItem("cars")) || [];
+
+  carList.innerHTML = "";
+
+  cars.forEach(function (car, index) {
+    const newItem = document.createElement("li");
+
+    newItem.textContent = `Marka: ${car.brand} | Model: ${car.model} | Rok: ${car.year} | Przebieg: ${car.mileage} km | Cena: ${car.price} zł`;
+
+    newItem.classList.add("new-li");
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Usuń";
+    deleteButton.classList.add("delete-button");
+
+    newItem.appendChild(deleteButton);
+    carList.appendChild(newItem);
+
+    deleteButton.addEventListener("click", function () {
+      const cars = JSON.parse(localStorage.getItem("cars")) || [];
+
+      cars.splice(index, 1);
+
+      localStorage.setItem("cars", JSON.stringify(cars));
+
+      displayData();
+    });
+  });
+};
+
 addCarButton.addEventListener("click", function () {
   if (
     brand.value.trim() === "" ||
@@ -15,30 +46,49 @@ addCarButton.addEventListener("click", function () {
     mileage.value.trim() === "" ||
     price.value.trim() === ""
   ) {
-    alert("Wpisz dane");
+    alert("Wpisz wszystkie dane");
     return;
   }
-  const newItem = document.createElement("li");
-  newItem.textContent = `Marka: ${brand.value} | Model: ${model.value} | Rok: ${year.value} | Przebieg: ${mileage.value} | Cena: ${price.value}`;
-  newItem.classList.add("new-li");
-  carList.appendChild(newItem);
+
+  const newCar = {
+    brand: brand.value.trim(),
+    model: model.value.trim(),
+    year: year.value,
+    mileage: mileage.value,
+    price: price.value,
+  };
+
+  const cars = JSON.parse(localStorage.getItem("cars")) || [];
+
+  cars.push(newCar);
+
+  localStorage.setItem("cars", JSON.stringify(cars));
+
+  displayData();
+
   brand.value = "";
   model.value = "";
   year.value = "";
   mileage.value = "";
   price.value = "";
+
   brand.focus();
-
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Usuń";
-  deleteButton.classList.add("delete-button");
-  newItem.appendChild(deleteButton);
-
-  deleteButton.addEventListener("click", function () {
-    deleteButton.parentElement.remove();
-  });
 });
 
 searchInput.addEventListener("input", function () {
-  console.log(searchInput.value);
+  const searchedText = searchInput.value.toLowerCase();
+
+  const allCars = document.querySelectorAll("#car-list li");
+
+  allCars.forEach(function (car) {
+    const carText = car.textContent.toLowerCase();
+
+    if (carText.includes(searchedText)) {
+      car.style.display = "block";
+    } else {
+      car.style.display = "none";
+    }
+  });
 });
+
+displayData();
